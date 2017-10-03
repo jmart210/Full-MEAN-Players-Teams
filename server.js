5ac9aa8e064060b8741cc7516e0e60131b22d810
jmart210 <crusader11jm@yahoo.com>
2017-10-03 14:33:04 -0500

    add routing/controller, still need to remove team..

--- server.js
+++ server.js
@@ -1,25 +1,129 @@
 var express = require("express"),
     app = express(),
     bodyParser = require("body-parser"),
-    mongoose = require("mongoose"),
     PORT = 8000;
 
-mongoose.connect("mongodb://localhost/full_player_teams", {useMongoClient: true});
+app.use(bodyParser.json());
+
+// DATABASE CONNECTION, SCHEMAS AND MODELS
+var mongoose = require("mongoose");
+mongoose.connect('mongodb://localhost/full_players_teams', {useMongoClient: true});
 mongoose.Promise = global.Promise;
 
-
-var teamSchema = new mongoose.Schema({
+var TeamSchema = new mongoose.Schema({
     name: String
 });
 
-var playerSchema = new mongoose.Schema({
+var PlayerSchema = new mongoose.Schema({
     name: String,
     team: { type: mongoose.Schema.Types.ObjectId, ref:'Team' }
 });
 
-mongoose.model('Team', teamSchema);
-mongoose.model('Players', playerSchema);
+var Team = mongoose.model('Team', TeamSchema);
+var Player = mongoose.model('Player', PlayerSchema);
 
+
+// PLAYER CONTROLLER
+var PlayerController = {
+    index: function(req, res){
+        Player.find({})
+        .populate('team')
+        .exec(function(err, players){
+            if(err){
+                console.log(err);
+                res.status(500);
+                res.json(err);
+            } else {
+                res.json(players);
+            }
+        });
+    },
+    create: function(req, res){
+        Player.create(req.body, function(err){
+            if(err){
+                console.log(err);
+                res.status(500);
+                res.json(err);
+            } else {
+                res.json(true);
+            }
+        });
+    },
+    delete: function(req, res){
+        Player.remove({_id: req.params.id })
+        .then(function(){
+            res.json(true);
+        })
+        .catch(function(err){
+            console.log(err);
+            res.status(500);
+            res.json(err);
+        });
+    },
+    update: function(req, res){
+        Player.update({ _id: req.params.id }, req.body)
+        .then(function(){
+            res.json(true);
+        })
+        .catch(function(err){
+            console.log(err);
+            res.status(500);
+            res.json(err);
+        });
+    },
+}
+
+// Team CONTROLLER
+var TeamController = {
+    index: function(req, res){
+        Team.find({}, function(err, teams){
+            if(err){
+                console.log(err);
+                res.status(500);
+                res.json(err);
+            } else {
+                res.json(teams);
+            }
+        });
+    },
+    create: function(req, res){
+        Team.create(req.body, function(err){
+            if(err){
+                console.log(err);
+                res.status(500);
+                res.json(err);
+            } else {
+                res.json(true);
+            }
+        });
+    },
+    delete: function(req, res){
+        Team.remove({_id: req.params.id })
+        .then(function(){
+            res.json(true);
+        })
+        .catch(function(err){
+            console.log(err);
+            res.status(500);
+            res.json(err);
+        });
+    },
+}
+
+
+// ROUTING
+// Players
+app.get('/players', PlayerController.index);
+app.post('/players', PlayerController.create);
+app.delete('/players/:id', PlayerController.delete);
+app.put('/players/:id', PlayerController.update);
+
+
+
+// Teams 
+app.get('/teams', TeamController.index);
+app.post('/teams', TeamController.create);
+app.delete('/teams/:id', TeamController.delete);
 
 
 
